49524738315aeaf579a83266116bb9129e490bd6
Starlynne <lewis.start0905@gmail.com>
2018-12-18 16:33:33 -0800

    Fix links, add jquery for spaceIvaders canvas

---
+++ spaceInvaders/spaceInvaders.js
@@ -0,0 +1,7 @@
+var CANVAS_WIDTH = 480;
+var CANVAS_HEIGHT = 320;
+
+var canvasElement = $("<canvas width='" + CANVAS_WIDTH +
+  "' height='" + CANVAS_HEIGHT + "'></canvas>");
+var canvas = canvasElement.get(0).getContext("2d");
+canvasElement.appendTo("#spaceCanvas");
